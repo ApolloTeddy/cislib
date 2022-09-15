d7cdfe45cead43648748e4f299874c377a87e627
ApolloTeddy <avzaviercallen@gmail.com>
2022-09-14 20:04:40 -0400

    Updates
    - DFT implemented
    - Performance shifting
    - bug fixes

--- src/CiSlib/CNum.java
+++ src/CiSlib/CNum.java
@@ -1,20 +1,16 @@
 package CiSlib;
 
 public class CNum {
-	private final float TAU = (float)Math.PI*2;
-	protected float re, im, th, am;
+	protected double re, im, th, am;
 	
-	protected CNum(float[] info) {
+	protected CNum(double[] info) {
 		this.re = info[0]; this.im = info[1];
-		this.th = info[2]%TAU; this.am = info[3]*info[3];
+		this.th = info[2]; this.am = info[3];
 	}
 	
-	private void reCalcPol() { this.th = (float)Math.atan2(this.im, this.re); this.am = this.re*this.re+this.im*this.im; }
+	private void reCalcPol() { this.th = Math.atan2(this.im, this.re); this.am = Math.sqrt(this.re*this.re+this.im*this.im); }
 	
-	private void reCalcCar() { 
-		float am = (float)Math.sqrt(this.am);
-		this.re = am*(float)Math.cos(this.th); this.im = am*(float)Math.sin(this.th); 
-	}
+	private void reCalcCar() { this.re = this.am*Math.cos(this.th); this.im = this.am*Math.sin(this.th); }
 	
 	public void add(CNum b) {
 		this.re += b.re; this.im += b.im;
@@ -27,23 +23,23 @@ public class CNum {
 	}
 	
 	public void mult(CNum b) {
-		this.th = (this.th+b.th)%TAU; this.am *= b.am;
+		this.th = (this.th+b.th); this.am *= b.am;
 		reCalcCar();
 	}
 	
 	public void div(CNum b) {
-		this.th = (this.th-b.th)%TAU; this.am /= b.am;
+		this.th = (this.th-b.th); this.am /= b.am;
 		reCalcCar();
 	}
 	
-	public float[] get() { return new float[]{this.re, this.im, this.th, (float)Math.sqrt(this.am)}; }
+	public double[] get() { return new double[]{this.re, this.im, this.th, this.am}; }
 	
-	public void setP(float th, float am) {
-		this.th = th%TAU; this.am = am*am;
+	public void setP(double th, double am) {
+		this.th = th; this.am = am;
 		reCalcCar();
 	}
 	
-	public void setC(float re, float im) {
+	public void setC(double re, double im) {
 		this.re = re; this.im = im;
 		reCalcPol();
 	}
--- src/CiSlib/CiSMath.java
+++ src/CiSlib/CiSMath.java
@@ -2,6 +2,22 @@ package CiSlib;
 
 public class CiSMath {
 	public static final CNum i = fromCart(0, 1);
+	private static final double TAU = Math.PI * 2;
+	
+	public static CNum[] DFT(CNum[] x_n) {
+		int N = x_n.length;
+		CNum[] X = new CNum[N];
+		
+		for(int k = 0; k < N; k++) {
+			CNum tmp = new CNum(new double[]{0, 0, 0, 0});
+			
+			for(int n = 0; n < N; n++) {
+				tmp.add(mult(x_n[n], fromPolar(-(TAU * k * n)/N, 1)));
+			}
+			X[k] = tmp;
+		}
+		return X;
+	}
 	
 	public static CNum add(CNum a, CNum b) {
 		return fromCart(a.re + b.re, a.im + b.im);
@@ -20,14 +36,14 @@ public class CiSMath {
 	}
 	
 	public static CNum rootOfUnity(int root) {
-		return fromPolar((2*(float)Math.PI)/root, 1);
+		return fromPolar(TAU/root, 1);
 	}
 	
-	public static CNum fromPolar(float th, float am) {
-		return new CNum(new float[]{am*(float)Math.cos(th), am*(float)Math.sin(th), th, am});
+	public static CNum fromPolar(double th, double am) {
+		return new CNum(new double[]{am*Math.cos(th), am*Math.sin(th), th, am});
 	}
 
-	public static CNum fromCart(float re, float im) {
-		return new CNum(new float[]{re, im, (float)Math.atan(im/re), (float)Math.sqrt(re*re+im*im)});
+	public static CNum fromCart(double re, double im) {
+		return new CNum(new double[]{re, im, Math.atan2(im, re), Math.sqrt(re*re+im*im)});
 	}
 }
